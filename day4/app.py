b9d4702abf533f0a4d4f43379c3c1157d0eeea1f
zhaofangfang1991 <helloworldzff@163.com>
2021-08-30 13:55:42 +0800

    重新学习第三章-教程

--- day4/app.py
+++ day4/app.py
@@ -1,9 +1,9 @@
 from flask import Flask,render_template,flash
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = b'_5#y2L"F4Q8z*^#/'  # secret_key
 
-@app.route('/index')
-def index():
-    flash('这是flash:闪现一个消息')
-    return render_template('index.html')
+@app.route('/index') # 路由
+def index(): # 视图函数
+    flash('这是flash:闪现一个消息')  # 消息闪现
+    return render_template('index.html')  # 渲染模板
